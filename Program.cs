17738c74ade6817bd95d7f557b99461b0e7b475c
Phsimmer <pedrohsimmer@gmail.com>
2024-05-21 02:08:37 -0300

    Entrega do Desafio Abstraindo um Celular - Pedro Simmer

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero:"456",modelo:"modelo1",imei:"22222222",memoria:32);
+Smartphone iphone = new Iphone(numero:"123",modelo:"modelo2",imei:"11111111",memoria:128);
+
+Console.WriteLine($"Telefone NOKIA");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Telefone IPHONE :");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whats'up");
+
